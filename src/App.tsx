87e4972145e814e82eb15fd673b3bfdfd9266be9
t.miljkovic <tarik.miljkovic@convotis.com>
2023-09-07 20:52:15 +0200

    awd

--- src/App.tsx
+++ src/App.tsx
@@ -352,10 +352,10 @@ function App() {
         <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
             <h2 className="text-base font-semibold leading-7 text-blue-600">
-              Deploy faster
+              FÜR UNTERNEHMEN | COACHES | KEYNOTE SPEAKER
             </h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Planning. Web. App. Marketing.
+              Consulting. Web Design. Mobile App Entwicklung. Marketing.
             </p>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Ein Vierfach notwendig für den kompletten Kreis. Es ist uns
@@ -392,16 +392,14 @@ function App() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-4xl text-center">
               <h2 className="text-base font-semibold leading-7 text-blue-600">
-                Preispläne für alle Bedürfnisse
+                Webseiten
               </h2>
               <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                Preispläne für alle Bedürfnisse&nbsp;
+                Professionelles Webdesign passend zum Budget &nbsp;
               </p>
             </div>
             <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
-              Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
-              quasi iusto modi velit ut non voluptas in. Explicabo id ut
-              laborum.
+              Preispläne für alle Bedürfnisse
             </p>
             <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               {tiers.map((tier, tierIdx) => (
